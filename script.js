541fd0c1efefcd55253df376e21efe658021a24d
Akhvleda1 <luka.akhvlediani2504@gmail.com>
2025-02-02 13:46:49 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,334 @@
+const TMDB_API_KEY = "3a88686a5740ae02c9bb094950cbed22";
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/";
+const IMG_SIZE = "w500";
+
+// APPEND BOXES WITH POPULAR MOVIES
+function generatePopularMovieBoxes(movies) {
+    let fourthContainer = document.querySelector(".fourth-box-container");
+
+    movies.forEach((movie) => {
+        let fourthBox = document.createElement("div");
+        fourthBox.classList.add("fourth-box");
+
+        fourthBox.addEventListener("click", function () {
+            window.location.href = `about.html?movieId=${movie.id}`;
+        });
+
+        let fourthBoxImg = document.createElement("img");
+        fourthBoxImg.src = `${IMG_BASE_URL}${IMG_SIZE}${movie.poster_path}`;
+
+        let fourthBoxTextDiv = document.createElement("div");
+        fourthBoxTextDiv.classList.add("fourth-box-text-div");
+
+        let fourthBoxRatingDiv = document.createElement("div");
+        fourthBoxRatingDiv.classList.add("fourth-box-rating-container");
+
+        let fourthBoxStarIcon = document.createElement("i");
+        fourthBoxStarIcon.classList.add("fa-solid", "fa-star");
+        fourthBoxStarIcon.style.color = "#FFD43B";
+
+        let fourthBoxRating = document.createElement("p");
+        fourthBoxRating.textContent = movie.vote_average;
+        fourthBoxRating.classList.add("fourth-box-rating");
+
+        let fourthBoxTitle = document.createElement("h2");
+        fourthBoxTitle.textContent = movie.original_title;
+
+        fourthBoxRatingDiv.append(fourthBoxStarIcon);
+        fourthBoxRatingDiv.append(fourthBoxRating);
+        fourthBoxTextDiv.append(fourthBoxRatingDiv);
+        fourthBoxTextDiv.append(fourthBoxTitle);
+        fourthBox.append(fourthBoxImg);
+        fourthBox.append(fourthBoxTextDiv);
+
+        fourthContainer.append(fourthBox);
+    });
+}
+
+// APPEND BOXES WITH TOP RATED MOVIES
+function generateTopRatedMoviesTMDB(movies) {
+    let tpMain = document.querySelector(".tp-main");
+
+    movies.forEach((movie, index) => {
+        let tpBox = document.createElement("div");
+        tpBox.classList.add("tp-box");
+
+        tpBox.addEventListener("click", function () {
+            window.location.href = `about.html?movieId=${movie.id}`;
+        });
+
+        let tpBoxImg = document.createElement("img");
+        tpBoxImg.src = `${IMG_BASE_URL}${IMG_SIZE}${movie.poster_path}`;
+        tpBoxImg.alt = movie.original_title;
+
+        let tpBoxTextDiv = document.createElement("div");
+        tpBoxTextDiv.classList.add("tp-box-text-div");
+
+        let tpBoxRatingDiv = document.createElement("div");
+        tpBoxRatingDiv.classList.add("tp-box-rating-container");
+
+        let tpBoxStarIcon = document.createElement("i");
+        tpBoxStarIcon.classList.add("fa-solid", "fa-star");
+
+        let tpBoxRating = document.createElement("p");
+        tpBoxRating.textContent = movie.vote_average;
+        tpBoxRating.classList.add("tp-box-rating");
+
+        let tpBoxTitle = document.createElement("h2");
+        tpBoxTitle.textContent = movie.original_title;
+
+        tpBoxRatingDiv.append(tpBoxStarIcon, tpBoxRating);
+        tpBoxTextDiv.append(tpBoxRatingDiv, tpBoxTitle);
+        tpBox.append(tpBoxImg, tpBoxTextDiv);
+
+        tpMain.append(tpBox);
+    });
+}
+
+// FETCH MOVIE DETAILS FOR ABOUT PAGE
+function displayMovieDetails(movie) {
+    let movieDetailsContainer = document.querySelector(".about-main");
+
+    let moviePosterDiv = movieDetailsContainer.querySelector(".about-pic-div");
+    let moviePoster = moviePosterDiv.querySelector("img");
+    moviePoster.src = `${IMG_BASE_URL}${IMG_SIZE}${movie.poster_path}`;
+
+    let descriptionDiv = movieDetailsContainer.querySelector(".descr-div");
+
+    let movieTitle = descriptionDiv.querySelector(".about-movie-title");
+    movieTitle.textContent = movie.original_title;
+    let movieRating = descriptionDiv.querySelector(".about-movie-rating");
+    movieRating.innerHTML = `Rating: ${movie.vote_average}/10 <span><i class="fa-solid fa-star"></i></span>`;
+    let movieRuntime = descriptionDiv.querySelector(".about-movie-runtime");
+    movieRuntime.textContent = `Runtime: ${movie.runtime} min`;
+    let movieRdate = descriptionDiv.querySelector(".about-movie-rdate");
+    movieRdate.textContent = `Release Date: ${movie.release_date}`;
+    let movieGenre = descriptionDiv.querySelector(".about-movie-genre");
+    movieGenre.textContent = `Genres: ${movie.genres
+        .map((genre) => genre.name)
+        .join(", ")}`;
+    let movieoverview = descriptionDiv.querySelector(".overview");
+    movieoverview.innerHTML = `Overview: <br/>${movie.overview}`;
+}
+function fetchMovieById(movieId) {
+    let TMDB_URL = `${TMDB_BASE_URL}/movie/${movieId}?api_key=${TMDB_API_KEY}&language=en-US`;
+    fetch(TMDB_URL, {
+        method: "GET",
+    })
+        .then((response) => response.json())
+        .then((movie) => {
+            displayMovieDetails(movie);
+        });
+}
+
+// Fetch movies by search
+function displaySearchResults(movies) {
+    let searchMain = document.querySelector(".search-main");
+
+    movies.forEach((movie, index) => {
+        let searchBox = document.createElement("div");
+        searchBox.classList.add("search-box");
+
+        searchBox.addEventListener("click", function () {
+            window.location.href = `about.html?movieId=${movie.id}`;
+        });
+
+        let searchBoxImg = document.createElement("img");
+        searchBoxImg.src = `${IMG_BASE_URL}${IMG_SIZE}${movie.poster_path}`;
+        searchBoxImg.alt = movie.original_title;
+
+        let searchBoxTextDiv = document.createElement("div");
+        searchBoxTextDiv.classList.add("search-box-text-div");
+
+        let searchBoxRatingDiv = document.createElement("div");
+        searchBoxRatingDiv.classList.add("search-box-rating-container");
+
+        let searchBoxStarIcon = document.createElement("i");
+        searchBoxStarIcon.classList.add("fa-solid", "fa-star");
+
+        let searchBoxRating = document.createElement("p");
+        searchBoxRating.textContent = movie.vote_average;
+        searchBoxRating.classList.add("search-box-rating");
+
+        let searchBoxTitle = document.createElement("h2");
+        searchBoxTitle.textContent = movie.original_title;
+
+        searchBoxRatingDiv.append(searchBoxStarIcon, searchBoxRating);
+        searchBoxTextDiv.append(searchBoxRatingDiv, searchBoxTitle);
+        searchBox.append(searchBoxImg, searchBoxTextDiv);
+
+        searchMain.append(searchBox);
+    });
+}
+let currentSearchPage = 1;
+function fetchMovieByQuery(query, page = 1) {
+    let TMDB_URL = `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(
+        query
+    )}&language=en-US&page=${page}`;
+    fetch(TMDB_URL, {
+        method: "GET",
+    })
+        .then((response) => response.json())
+        .then((movie) => {
+            if (movie.results.length > 0) {
+                displaySearchResults(movie.results);
+            } else {
+                console.log("no search found");
+            }
+        });
+}
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get("query");
+if (query) {
+    fetchMovieByQuery(query, currentSearchPage);
+}
+// LOAD MORE BUTTON FUNCTIONALITY
+let loadMoreButtonSearch = document.getElementById("searchLoadMoreButton");
+if (loadMoreButtonSearch) {
+    loadMoreButtonSearch.addEventListener("click", function () {
+        currentSearchPage++; 
+        fetchMovieByQuery(query, currentSearchPage); 
+    });
+}
+
+
+// GET POPULAR MOVIES DATA
+function fetchPopularMoviesTMDB() {
+    let TMDB_URL = `${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`;
+    fetch(TMDB_URL, {
+        method: "GET",
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJson) {
+            let movies = responseJson.results;
+            generatePopularMovieBoxes(movies.slice(0, 10));
+        });
+}
+
+// GET TOP RATED MOVIES DATA
+let currentPage = 1;
+function fetchTopRatedMoviesTMDB(page) {
+    let TMDB_URL = `${TMDB_BASE_URL}/movie/top_rated?api_key=${TMDB_API_KEY}&language=en-US&page=${page}`;
+    fetch(TMDB_URL, {
+        method: "GET",
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJson) {
+            let movies = responseJson.results;
+            generateTopRatedMoviesTMDB(movies);
+        });
+}
+
+// LOAD MORE BUTTON FUNCTIONALITY
+let loadMoreButton = document.getElementById("loadMoreButton");
+if (loadMoreButton) {
+    loadMoreButton.addEventListener("click", function () {
+        currentPage++;
+        fetchTopRatedMoviesTMDB(currentPage);
+    });
+}
+
+// SHOW/HIDE PASSWORD FUNCTIONALITY
+let passwordInput = document.querySelector(".password-input");
+let showPasswordButton = document.querySelector(".show-password-btn");
+if (passwordInput && showPasswordButton) {
+    showPasswordButton.addEventListener("click", function () {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            showPasswordButton.textContent = "hide password";
+        } else {
+            passwordInput.type = "password";
+            showPasswordButton.textContent = "show password";
+        }
+    });
+}
+
+// FORM VALIDATION
+let signInForm = document.getElementById("signin-form");
+if (signInForm) {
+    signInForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        let errors = {};
+
+        let signInEmail = document.querySelector(".email-input");
+        if (signInEmail.value === "") {
+            errors.email = "Email field can not be empty";
+        }
+        let signInPass = document.querySelector(".password-input");
+        if (signInPass.value === "") {
+            errors.pass = "Password field can not be empty";
+        }
+
+        signInForm.querySelectorAll(".error-text").forEach((el) => {
+            el.textContent = " ";
+        });
+
+        for (let item in errors) {
+            let pErrorElement = document.getElementById("error-" + item);
+            if (pErrorElement) {
+                pErrorElement.textContent = errors[item];
+            }
+        }
+        if (Object.keys(errors).length === 0) {
+            signInForm.submit();
+        }
+    });
+}
+
+// WHEN PAGE LOADS
+document.addEventListener("DOMContentLoaded", function () {
+    // burger functionality
+    let burgerDiv = document.querySelector(".burger-div");
+    if (burgerDiv) {
+        burgerDiv.addEventListener("click", function () {
+            document.querySelector(".off-screen-menu").classList.toggle("open");
+        });
+    }
+
+    // fetch movies if containers exist to avoid errors
+    if (document.querySelector(".fourth-box-container")) {
+        fetchPopularMoviesTMDB();
+    }
+    if (document.querySelector(".tp-main")) {
+        fetchTopRatedMoviesTMDB(currentPage);
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const movieId = urlParams.get("movieId");
+
+    if (movieId) {
+        fetchMovieById(movieId);
+    }
+});
+
+// Implement search functionality
+document.addEventListener("DOMContentLoaded", () => {
+    function handleSearch(inputSelector, ButtonSelector) {
+        const input = document.querySelector(inputSelector);
+        const button = document.querySelector(ButtonSelector);
+
+        if (input && button) {
+            button.addEventListener("click", () => {
+                const query = input.value.trim();
+                if (query) {
+                    window.location.href = `search.html?query=${encodeURIComponent(
+                        query
+                    )}`;
+                }
+            });
+            input.addEventListener("keypress", (event) => {
+                if (event.key === "Enter") {
+                    event.preventDefault();
+                    button.click();
+                }
+            });
+        }
+    }
+    handleSearch(".search-input", ".search-button");
+    handleSearch(".os-search-input", ".os-search-button");
+});
---
+++ webcomponents.js
@@ -0,0 +1,99 @@
+// header-component.js
+class HeaderComponent extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <section class="header-section">
+            <div class="header-div">
+                <div class="logo-nav-wrapper">
+                    <img class="header-logo" src="images/logo.png" alt="logo" />
+
+                    <nav class="header-nav">
+                        <ul class="header-ul">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="toprated.html">Top Rated</a></li>
+                            <li><a href="signin.html">Sign In</a></li>
+                        </ul>
+                    </nav>
+
+                    <div class="off-screen-menu">
+                        <ul class="off-screen-ul">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="toprated.html">Top Rated</a></li>
+                            <li><a href="signin.html">Sign In</a></li>
+                            <li>
+                                <input
+                                    class="os-search-input"
+                                    placeholder="Search for any movie"
+                                    type="text"
+                                />
+                                <button class="os-search-button">
+                                    <i
+                                        class="fa-solid fa-magnifying-glass fa-xl"
+                                    ></i>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="burger-div">
+                        <i class="fa-solid fa-bars fa-xl menu-icon"></i>
+                    </div>
+                </div>
+                <div class="search-wrapper">
+                    <input
+                        class="search-input"
+                        placeholder="Search for any movie"
+                        type="text"
+                    />
+                    <button class="search-button">
+                        <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+                    </button>
+                </div>
+            </div>
+        </section>
+        `;
+    }
+}
+
+class FooterComponent extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <section class="footer-section">
+            <div class="footer-content">
+                <div class="footer-main">
+                    <div class="footer-main1">
+                        <img class="footer-logo" src="images/logo.png" alt="Logo">
+                        <p>328 Queensberry Street, North Melbourne VIC3051, <br>
+                            Australia.</p>
+                    </div>
+                    <div class="footer-main2">
+                        <a href="index.html"><p>Home</p></a>
+                        <a href="toprated.html"><p>Top Rated</p></a>
+                        <a href="signin.html"><p>Sign in</p></a>
+                    </div>
+                    <div class="footer-main3">
+                        <p>Services</p>
+                        <p>Contact</p>
+                        <p>Privacy Policy</p>
+                    </div>
+                    <div class="footer-main4">
+                        <p>Connect With Us</p>
+                        <div class="footer-icon-div">
+                            <i class="fa-brands fa-twitter fa-xl"></i>
+                            <i class="fa-brands fa-instagram fa-xl"></i>
+                            <i class="fa-brands fa-facebook-f fa-xl"></i>
+                            <i class="fa-brands fa-linkedin fa-xl"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-p-div">
+                    <p>© www.themoviedb.org. All right reserved.</p>
+                </div>
+            </div>
+        </section>
+        `;
+    }
+}
+
+customElements.define("header-component", HeaderComponent);
+customElements.define("footer-component", FooterComponent);
